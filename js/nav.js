1102a810927f8ab1c5078566d8b4acee05c9cba7
Sheterhand <159464347+Sheterhand@users.noreply.github.com>
2024-02-17 17:13:36 +0200

    big update

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,38 @@
-$(document).ready(() => {
-    $(".accordion").on('click', () => {
-        $(this).next().slideDown();
-        
+$(document).ready(function () {
+
+    // Store variables
+
+    var accordion_head = $('.accordion'),
+        accordion_body = $('.panel');
+
+    // Click function
+
+    accordion_head.on('click', function (event) {
+
+        // Disable header links
+
+        event.preventDefault();
+
+        var $this = $(this);
+        var isActive = $this.hasClass('active');
+
+        // Close all accordion sections
+        accordion_head.removeClass('active');
+        accordion_body.slideUp('normal', 'linear');
+
+        // Toggle the clicked accordion section
+        if (!isActive) {
+            $this.addClass('active');
+            $this.next().stop(true, true).slideDown('normal', 'linear');
+        }
     });
 
+    $('.main-nav img').on("mouseenter", () => {
+        $('.nav-logo').removeClass('hidden').addClass('visible');
+    });
 
-    // $('.accordion').on('click', () => {
-    //     $(this).hide();
+    $('.nav-logo').on("mouseleave", () => {
+        $('.nav-logo').removeClass('visible').addClass('hidden');
+    });
 
-    // $('.panel').on('mouseleave', () => {
-    //     $('.panel').slideUp();
-        
 });
